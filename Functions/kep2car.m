620156b774a71289451855c76e5a98d0a5c6d9e4
SteBe01 <stefano3.belletti@mail.polimi.it>
2023-11-18 18:15:31 +0100

    Name fix

--- Functions/kep2car.m
+++ Functions/kep2car.m
@@ -1,78 +1,59 @@
-function [a, e, i, OM, om, theta] = kep2car(rr, vv, mu)
+function [rr, vv] = kep2car(varargin)
 
-% Transformation from Cartesian state to orbital elements
+% Transformation from orbital elements to Cartesian state 
+% given n theta angles
 %
-% [a, e, i, OM, om, theta] = rv2parorb (rr, vv, mu)
+% [rr, vv] = parorb2rv(a, e, i, OM, om, theta, mu)
+% [rr, vv] = parorb2rv([a, e, i, OM, om, theta, mu])
+%
+% This function accepts both array of elements and elements
 %
 % Input arguments:
 % ----------------------------------------------------------------
-% rr            [3x1]   position vector                 [km]
-% vv            [3x1]   velocity vector                 [km/s]
-% mu            [1x1]   gravitational parameter         [km^3/s^2]
-% 
-% Output arguments:
-% -----------------------------------------------------------------
 % a             [1x1]   semi-major axis                 [km]
 % e             [1x1]   eccentricity                    [-]
 % i             [1x1]   inclination                     [rad]
 % OM            [1x1]   RAAN                            [rad]
 % om            [1x1]   pericenter anomaly              [rad]
-% theta         [1x1]   true anomaly                    [rad]
+% theta         [nx1]   true anomaly vector             [rad]
+% mu            [1x1]   gravitational parameters        [km^3/s^2]
+% 
+% Output arguments:
+% -----------------------------------------------------------------
+% rr            [3xn]   position vector                 [km]
+% vv            [3xn]   velocity vector                 [km/s]
 
-I = [1 0 0];
-J = [0 1 0];
-K = [0 0 1];
 
-r = norm(rr);                                       % Scalar distance 
-v = norm(vv);                                       % Scalar Velocity
-
-vr = dot(rr, vv)/r;                                 % Radial velocity
-
-E = v^2/2 - mu/r;                                   % Specific mechanical energy
-a = -mu/(2*E);                                      % Semi-major axis
-
-h = cross(rr,vv);                                   % Specific angular momentum
-
-i = wrapTo2Pi(acos(h(3)/norm(h)));                  % Inclination
-
-N = cross(K, h);                                    % Nodes line
-
-OM_check = i;
-while OM_check > pi
-    OM_check = OM_check - pi;
-end
-
-if OM_check== 0 
-    OM = 0;
+if nargin == 1 && length(varargin{1}) == 7
+    t = num2cell(varargin{1});
+    [a, e, i, OM, om, theta, mu] = deal(t{:});
+elseif nargin == 7
+    [a, e, i, OM, om, theta, mu] = varargin{1:end};
 else
-    if (N(2) < 0)
-        OM = wrapTo2Pi((2*pi) - acos(N(1)/norm(N)));
-    else
-        OM = wrapTo2Pi(acos(N(1)/norm(N)));
-    end
+    error('Incorrect data input, type "help car2kep"')
 end
 
-ee = cross(vv, h)/mu - rr/r;
-e = norm(ee);
-
-if OM_check == 0 && e < 1e-10       % APPROXIMATION HEEEEEEELP
-    om = 0;
-else
-    if e < 1e-10
-        om = 0;
-    else
-        if (ee(3) < 0)
-            om = wrapTo2Pi(2*pi - acos(dot(N,ee)/(norm(N)*e)));
-        else
-            om = wrapTo2Pi(acos(dot(N,ee)/(norm(N)*e)));
-        end
-    end
+if size(theta, 1) < size(theta, 2)
+    theta = theta';
 end
 
-if (vr < 0)
-    theta = wrapTo2Pi(2*pi - real(acos(dot(ee,rr)/(r*e))));
-else
-    theta = wrapTo2Pi(acos(dot(ee,rr)/(r*e)));
-end
+R_OM = [cos(OM) sin(OM) 0; -sin(OM) cos(OM) 0; 0 0 1];      % Rotation around z axis with angle OM
+R_i = [1 0 0; 0 cos(i) sin(i); 0 -sin(i) cos(i)];           % Rotation around x axis with angle i
+R_om = [cos(om) sin(om) 0; -sin(om) cos(om) 0; 0 0 1];      % Rotation around z axis with angle om
+
+Rot_matrix = real(R_OM'* R_i'*R_om');                       % Rotation matrix applies all rotations
+
+p = a*(1-e^2);                                              % Semi-latus rectus
+temp = p./(1+e*cos(theta));                                 % Radius trigonometric coefficient
+
+% Calculate radius and velocity in the Perifocal system
+r_PF = [temp.*cos(theta) temp.*sin(theta) temp.*0];
+v_PF = sqrt(mu/p) .* [-sin(theta) e+cos(theta) zeros(size(theta, 1), 1)];
+
+% Apply rotations to switch from perifocal to ECI system
+rr = Rot_matrix * r_PF';
+rr = rr';
+vv = Rot_matrix * v_PF';
+vv = vv';
 
 end
