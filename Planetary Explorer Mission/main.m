9e129b8d507ed0974001fe5755c6823f6b169a0a
SteBe01 <stefano3.belletti@mail.polimi.it>
2023-11-23 16:28:49 +0100

    Added task2

---
+++ Planetary Explorer Mission/main.m
@@ -0,0 +1,66 @@
+%% orbit data
+
+addpath("Functions\")
+
+clear, clc
+close all
+
+% orbit data - goup 2346
+orbit.a = 0.8016 * 1e4;
+orbit.e = 0.1678;
+orbit.i = deg2rad(50.3442);
+orbit.ratio_k = 12;
+orbit.ratio_m = 1;
+
+orbit.OM = deg2rad(0);
+orbit.om = deg2rad(0);
+
+% perturbation: J2 and Drag (cD = 2.1, A/M = 0.0171 m^2/kg)
+
+
+%% unperturbed 2bp orbit
+
+mu = astroConstants(13);
+
+T = 2*pi*sqrt( orbit.a^3/mu );
+tspan= linspace( 0, T, 100 );
+
+[r0, v0] = kep2car(orbit.a, orbit.e, orbit.i, orbit.OM, orbit.om, 0, mu);
+y0 = [ r0'; v0' ];
+
+options = odeset( 'RelTol', 1e-13, 'AbsTol', 1e-14 );
+[ T, Y ] = ode113( @(t,y) ode_2bp(t,y,mu), tspan, y0, options );
+
+figure()
+plot3( Y(:,1), Y(:,2), Y(:,3), '-' )
+xlabel('X [km]'); ylabel('Y [km]'); zlabel('Z [km]');
+
+title('Two-body problem orbit');
+axis equal, grid on,hold on
+earthPlot;
+plot3( Y(1,1), Y(1,2), Y(1,3), 'or' )
+
+
+%% ground track plot (unperturbed)
+
+R_E = astroConstants(23);
+om_E = 15.04;                   % deg/h
+theta_g = 0;                    % theta Greenwich (t0)
+mu = astroConstants(13);
+orbit_number = orbit.ratio_k*10;
+tspan_dim = 100000;
+
+T = 2*pi*sqrt( orbit.a^3/mu );
+tspan= linspace( 0, T, tspan_dim );
+[r0, v0] = kep2car(orbit.a, orbit.e, orbit.i, orbit.OM, orbit.om, 0, mu);
+y0 = [ r0'; v0' ];
+[~, ~, lon, lat] = groundTrack_cart(y0, tspan*orbit_number, mu, theta_g, om_E);
+groundTrackPlot(lon, lat, "EarthTexture.jpg")
+
+orbit.a_rep = aFinder(orbit.ratio_k, orbit.ratio_m, om_E, mu);
+T = 2*pi*sqrt( orbit.a_rep^3/mu );
+tspan= linspace( 0, T, tspan_dim );
+[r0, v0] = kep2car(orbit.a_rep, orbit.e, orbit.i, orbit.OM, orbit.om, 0, mu);
+y0 = [ r0'; v0' ];
+[~, ~, lon, lat] = groundTrack_cart(y0, tspan*orbit_number, mu, theta_g, om_E);
+groundTrackPlot(lon, lat, "EarthTexture.jpg")
