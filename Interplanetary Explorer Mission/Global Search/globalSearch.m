ded167e5a9cfe80fd190213a15f581ee16310d16
SteBe01 <stefano3.belletti@mail.polimi.it>
2023-12-17 22:26:07 +0100

    Update to gloabSearch.m, parpool fix and efficiency improvements

--- Interplanetary Explorer Mission/Global Search/globalSearch.m
+++ Interplanetary Explorer Mission/Global Search/globalSearch.m
@@ -12,19 +12,30 @@ departure.planetId = 6;
 flyby.planetId = 5;
 arrival.bodyId = 79;
 
-n_elements = 1e4;
+n_elements = 1e3;
+parallel = 1;
 
 mission.dep_time = [2028 01 01 0 0 0];
 mission.arr_time = [2058 01 01 0 0 0];
 
 f = @(vect) completeInterplanetaryMS(vect(1), vect(2), vect(3), departure.planetId, flyby.planetId, arrival.bodyId);
-
-r1 = date2mjd2000(mission.dep_time) + (date2mjd2000(mission.arr_time)-date2mjd2000(mission.dep_time)).*rand(n_elements,1);
-r2 = date2mjd2000(mission.dep_time) + (date2mjd2000(mission.arr_time)-date2mjd2000(mission.dep_time)).*rand(n_elements,1);
-r3 = date2mjd2000(mission.dep_time) + (date2mjd2000(mission.arr_time)-date2mjd2000(mission.dep_time)).*rand(n_elements,1);
 x0 = date2mjd2000(mission.dep_time) + (date2mjd2000(mission.arr_time)-date2mjd2000(mission.dep_time)).*rand(3,1);
 
+tic
+
+r1 = zeros(n_elements, 1);
+r2 = r1;
+r3 = r1;
+for i = 1:n_elements
+    rand1 = date2mjd2000(mission.dep_time) + (date2mjd2000(mission.arr_time)-date2mjd2000(mission.dep_time)).*rand(1,1);
+    rand2 = rand1 + (date2mjd2000(mission.arr_time)-rand1).*rand(1,1);
+    rand3 = rand2 + (date2mjd2000(mission.arr_time)-rand2).*rand(1,1);
+    r1(i, :) = rand1;
+    r2(i, :) = rand2;
+    r3(i, :) = rand3;
+end
 stpts = [r1, r2, r3];
+
 startpts = CustomStartPointSet(stpts);
 lb = date2mjd2000(mission.dep_time);
 ub = date2mjd2000(mission.arr_time);
@@ -32,11 +43,25 @@ ub = date2mjd2000(mission.arr_time);
 opts = optimoptions(@fminunc, 'Algorithm', 'quasi-newton');
 newprob = createOptimProblem('fminunc', 'x0', x0, 'lb', lb, 'ub', ub, 'objective', f, 'options', opts);
 
-gs = GlobalSearch("Display","iter");
-ms = MultiStart(gs);
+gs = GlobalSearch;
+
+if parallel
+    ms = MultiStart(gs,'UseParallel', true, 'Display', 'iter');
+    pool = gcp('nocreate');
+    if isempty(pool)
+        parpool
+    else
+        warning("Using existing parpool")
+    end
+else
+    ms = MultiStart(gs,'UseParallel', false, 'Display', 'iter');
+end
 
 [xcust, fcust] = run(ms, newprob, startpts);
 
 [dv1, dv2, dv3, rp, exitValue] = completeInterplanetary(xcust(1), xcust(2), xcust(3), departure.planetId, flyby.planetId, arrival.bodyId);
 dvTot = dv1 + dv2 + dv3;
+
+time_elapsed = toc;
+disp("Time elapsed: " + time_elapsed + " s")
 
